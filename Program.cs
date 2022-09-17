d8207fd156d3fe99d6170c4e7b12b94e11ba6b58
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-09-17 16:17:48 +0200

    add fattoriale

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("scrivi un numero");
+long N = Convert.ToInt64(Console.ReadLine());
+
+long fattoriale(long N)
+{
+    if( N<= 0)
+    {
+        return 1;
+    }
+    {
+        return N * fattoriale(N - 1);
+    }
+
+}
+Console.WriteLine($"il fattoriale è {fattoriale(N)}");
+
